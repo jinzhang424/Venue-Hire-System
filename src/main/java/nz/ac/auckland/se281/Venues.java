fe88b26016c131508f3f795b15d027696ec24825
Jin <ajhz011@aucklanduni.ac.nz>
2024-03-16 12:21:50 +1300

    Set the fields in Venues to private and removed all the list related things

--- src/main/java/nz/ac/auckland/se281/Venues.java
+++ src/main/java/nz/ac/auckland/se281/Venues.java
@@ -3,10 +3,10 @@ package nz.ac.auckland.se281;
 import com.github.houbb.data.struct.core.util.list.ArrayList;
 
 public class Venues {
-  public String VENUE_NAME = null;
-  public String VENUE_CODE = null;
-  public String VENUE_CAPACITY = null;
-  public String HIRE_FEE = null;
+  private String VENUE_NAME = null;
+  private String VENUE_CODE = null;
+  private String VENUE_CAPACITY = null;
+  private String HIRE_FEE = null;
 
   
   public Venues(String venueName, String venueCode, String venueCapacity, String hireFee) {
@@ -16,15 +16,5 @@ public class Venues {
     this.VENUE_CAPACITY = venueCapacity;
     this.HIRE_FEE = hireFee;
     
-  }
-  
-  private ArrayList<Venues> VenueList = new ArrayList<Venues>(); 
-
-  public void addVenueToList() {
-    VenueList.add(this);
-  }
-
-  public void getVenueSize() {
-    VenueList.size();
   }
 }
