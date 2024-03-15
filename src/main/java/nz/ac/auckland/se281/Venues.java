601c2a41b9aa053e990ec3d7820ae0def52c2656
Jin <ajhz011@aucklanduni.ac.nz>
2024-03-15 20:29:42 +1300

    corrected the  parameter names  in constructor Venues in class venues

--- src/main/java/nz/ac/auckland/se281/Venues.java
+++ src/main/java/nz/ac/auckland/se281/Venues.java
@@ -5,4 +5,13 @@ public class Venues {
   public int VENUE_CODE;
   public int VENUE_CAPACITY;
   public int HIRE_FEE;
+
+  public Venues(String venueName, int venueCode, int venueCapacity, int hireFee) {
+
+    VENUE_NAME = venueName;
+    VENUE_CODE = venueCode;
+    VENUE_CAPACITY = venueCapacity;
+    HIRE_FEE = hireFee;
+  }
+  
 }
