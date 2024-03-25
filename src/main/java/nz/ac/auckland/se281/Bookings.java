fad539775f157b1c395d9b74c137f0ba810a213a
Jin <ajhz011@aucklanduni.ac.nz>
2024-03-25 22:31:11 +1300

    Created the constructor for class Bookings

---
+++ src/main/java/nz/ac/auckland/se281/Bookings.java
@@ -0,0 +1,18 @@
+package nz.ac.auckland.se281;
+
+import java.util.List;
+
+public class Bookings {
+  
+  private String bookingVenueCode = null;
+  private String bookingVenueDate = null;
+  private String bookingVenueEmail = null;
+  private String numOfAttendees = null;
+
+  public Bookings (String[] BookingInfo) {
+    bookingVenueCode = BookingInfo[0];
+    bookingVenueDate = BookingInfo[1];
+    bookingVenueEmail = BookingInfo[2];
+    numOfAttendees = BookingInfo[3]; 
+  }
+}
